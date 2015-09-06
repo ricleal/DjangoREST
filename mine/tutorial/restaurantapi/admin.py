881818bdf12c683dbbd4fbdc20f27bcbe2f2fee5
Ricardo M. Ferraz Leal <ricleal@gmail.com>
2015-09-06 14:48:16 -0500

    customised admin interface display objects.

--- mine/tutorial/restaurantapi/admin.py
+++ mine/tutorial/restaurantapi/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
 from .models import Menu, MenuItem
 
-admin.site.register(Menu)
-admin.site.register(MenuItem)
+
+from django.contrib import admin
+from tutorial.restaurantapi.models import Menu, MenuItem
+
+# Register your models here.
+
+
+class MenuAdmin(admin.ModelAdmin):
+    # pass
+    list_display = ['id', 'name', 'description', 'chef', 'available']
+
+
+class MenuItemAdmin(admin.ModelAdmin):
+    #pass
+    # Collumns to display in the admin interface when listing all entries
+    list_display = ['id', 'name', 'description', 'cost_to_make',
+                    'sale_price', 'available', 'menu']
+
+
+admin.site.register(Menu, MenuAdmin)
+admin.site.register(MenuItem, MenuItemAdmin)
